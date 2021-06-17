166f1611a93fc7f81c36759760a513ded98cb06b
Alex Gilmer <alexgilmer@student.mitt.ca>
2021-06-17 13:53:57 -0500

    Add favoriting to Search page

--- src/components/PageSearch.jsx
+++ src/components/PageSearch.jsx
@@ -2,10 +2,20 @@ import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import Movie from './Movie';
 import { search } from '../utilities/tmdbAPI';
+import { setList, getList } from '../utilities/watchListAPI';
 
 const PageSearch = (props) => {
   const location = useLocation();
   const [shows, setShows] = useState([]);
+  const [favList, setFavList] = useState([]);
+
+  const updateFaves = () => {
+    setFavList(getList())
+  };
+
+  useEffect(() => {
+    updateFaves();
+  }, []);
 
   useEffect(() => {
     const query = new URLSearchParams(location.search).get('query');
@@ -15,13 +25,30 @@ const PageSearch = (props) => {
     });
   }, [location.search]);
 
+  const changeFav = (id) => {
+    const isOnList = favList.includes(id);
+    if (isOnList) {
+      setList(favList.filter(elem => elem !== id));
+    } else {
+      setList([...favList, id]);
+    }
+    updateFaves();
+  }
+
   return (
   <div className="titleList">
     <div className="title">
       <h1>Results</h1>
       <div className="titles-wrapper">
         {shows.map((show, index) => {
-          return <Movie item={show} key={index}/>;
+          return (
+            <Movie
+              item={show}
+              isOnList={favList.includes(show.id)}
+              changeFavCB={changeFav}
+              key={index}
+            />
+          );
         })}
       </div>
     </div>
